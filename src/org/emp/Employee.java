3c03dcccde53ce4cc722fba2d263a2c62cce3d47
seethajenifar <seethajenifar@gmail.com>
2024-01-29 16:26:07 +0530

    project structure

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,24 @@
+package org.emp;
+
+public class Employee {
+	
+private void Emp(){
+System.out.println("empID");
+}
+private void Emp1() {
+System.out.println("empName");
+}
+private void Emp2() {
+	System.out.println("empPhone");
+}
+private void Emp3() {
+System.out.println("empAddress");
+}
+public static void main (String[]args) {
+Employee emp = new Employee();
+emp.Emp();
+emp.Emp1();
+emp.Emp2();
+emp.Emp3();
+}
+}
